2530a5461b97a63a09264fe8b8056613514243ec
major101x <72233326+major101x@users.noreply.github.com>
2026-02-21 00:16:03 +0100

    refactor methods after learning difference between client and server

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -28,25 +28,76 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
     return data;
   }
 
-  @SubscribeMessage('join-pr')
-  async handleJoinPR(
+  @SubscribeMessage('join-pr-room')
+  async handleJoinPRRoom(
     @MessageBody() data: { pullRequestId: number },
     @ConnectedSocket() client: Socket,
   ) {
     await client.join(`pr:${data.pullRequestId}`);
-    client.emit('joined', `Joined PR ${data.pullRequestId}`);
-    this.server.emit('joined', `Joined PR ${data.pullRequestId}`);
+    client
+      .to(`pr:${data.pullRequestId}`)
+      .emit('joined', `User with client id ${client.id} has joined the chat`);
+    const clientsInRoom = this.server
+      .in(`pr:${data.pullRequestId}`)
+      .fetchSockets();
+    const userCount = (await clientsInRoom).length;
+    this.server
+      .to(`pr:${data.pullRequestId}`)
+      .emit('user-count', { userCount });
     return data;
   }
 
-  @SubscribeMessage('leave-pr')
-  async handleLeavePR(
+  @SubscribeMessage('leave-pr-room')
+  async handleLeavePRRoom(
     @MessageBody() data: { pullRequestId: number },
     @ConnectedSocket() client: Socket,
   ) {
     await client.leave(`pr:${data.pullRequestId}`);
-    client.emit('left', `Left PR ${data.pullRequestId}`);
-    this.server.emit('left', `Left PR ${data.pullRequestId}`);
+    this.server
+      .to(`pr:${data.pullRequestId}`)
+      .emit('left', `Left PR ${data.pullRequestId}`);
+    const clientsInRoom = this.server
+      .in(`pr:${data.pullRequestId}`)
+      .fetchSockets();
+    const userCount = (await clientsInRoom).length;
+    this.server
+      .to(`pr:${data.pullRequestId}`)
+      .emit('user-count', { userCount });
+    return data;
+  }
+
+  @SubscribeMessage('joined')
+  handleJoined(
+    @MessageBody() data: { pullRequestId: number },
+    @ConnectedSocket() client: Socket,
+  ) {
+    // this.server
+    //   .to(`pr:${data.pullRequestId}`)
+    //   .emit('joined', `Joined PR ${data.pullRequestId}`);
+    console.log('Joined PR', data.pullRequestId);
+    return data;
+  }
+
+  @SubscribeMessage('left')
+  handleLeft(
+    @MessageBody() data: { pullRequestId: number },
+    @ConnectedSocket() client: Socket,
+  ) {
+    // this.server
+    //   .to(`pr:${data.pullRequestId}`)
+    //   .emit('left', `Left PR ${data.pullRequestId}`);
+    console.log('Left PR', data.pullRequestId);
+    return data;
+  }
+
+  @SubscribeMessage('send-message-to-pr-room')
+  handleMessage(
+    @MessageBody() data: { message: string; pullRequestId: number },
+    @ConnectedSocket() client: Socket,
+  ) {
+    this.server
+      .to(`pr:${data.pullRequestId}`)
+      .emit('pr-room-message', { message: data.message, userId: client.id });
     return data;
   }
 }
