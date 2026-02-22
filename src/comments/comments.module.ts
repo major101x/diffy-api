261f21d02e8a9a874549726d6a6e97cfd0ad2544
major101x <72233326+major101x@users.noreply.github.com>
2026-02-22 23:55:59 +0100

    fix event module import

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
 import { PrismaService } from 'src/prisma.service';
-import { EventsGateway } from 'src/events/events.gateway';
+import { EventsModule } from 'src/events/events.module';
 
 @Module({
   controllers: [CommentsController],
-  providers: [CommentsService, PrismaService, EventsGateway],
+  providers: [CommentsService, PrismaService],
+  imports: [EventsModule],
 })
 export class CommentsModule {}
